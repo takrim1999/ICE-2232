0241ff8f42f60801f9b687c93b55fe7acc2f42a7
takrim1999 <takrimulalom@gmail.com>
2023-04-29 20:58:08 +0600

    completed question 1 from CT1 odd

--- CT1/solutions/odd/temperature.c
+++ CT1/solutions/odd/temperature.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 int main()
 {
-    int days = 0;
-    printf("How many days you want to record?");
-    scanf("%d" & days);
-    int TEMPERATURE[days] = {};
+    int days = 0, avg = 0;
+    printf("How many days you want to record?\n>");
+    scanf("%d", &days);
+    int TEMPERATURE[days];
 
+    // Recording the temperature for given days
+
+    for (int i = 0; i < days; i++)
+    {
+        printf("Day %d: ", i + 1);
+        scanf("%d", &TEMPERATURE[i]);
+
+        // Removing invalid temperature
+
+        if (TEMPERATURE[i] < 0)
+        {
+            printf("Invalid temperature\n");
+            i--;
+        }
+    }
+
+    // printing all the temperature with there indexes
+    printf("Temperatures with days:\n");
+    for (int i = 0; i < days; i++)
+    {
+        printf("Day %d : %d\n", i + 1, TEMPERATURE[i]);
+        avg = avg + TEMPERATURE[i];
+    }
+    // printing the average
+    printf("Rounded Average is %d\n", avg / days);
     return 0;
 }
